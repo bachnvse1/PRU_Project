6dd179d752acdb4ea148e747626c70d733ae80ed
longhp2504 <157264591+longhp2504@users.noreply.github.com>
2024-10-23 16:52:32 +0700

    them kamehameha

---
+++ My project/Assets/Sprites/Android18/Script/PlayerAttackKamehameha.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlayerAttackKamehameha : MonoBehaviour
+{
+	[SerializeField] private float attackCooldown;
+	[SerializeField] private Transform firePoint; // Sử dụng firePoint để Kamehameha xuất phát từ đây
+	[SerializeField] private GameObject kamePrefab; // Prefab của Kamehameha
+
+	private Animator anim;
+	private float cooldownTimer = Mathf.Infinity;
+
+	private void Awake()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	private void Update()
+	{
+		if (Input.GetKey(KeyCode.L) && cooldownTimer > attackCooldown)
+			Attack();
+
+		cooldownTimer += Time.deltaTime;
+	}
+
+	private void Attack()
+	{
+		cooldownTimer = 0;
+		anim.SetTrigger("attack_kame");
+		Debug.Log("Kamehameha Attack Triggered");
+
+		// Tạo mới một Kamehameha từ prefab tại vị trí firePoint
+		GameObject newKame = Instantiate(kamePrefab, firePoint.position, firePoint.rotation);
+
+		// Gọi hàm SetDirection để thiết lập hướng cho Kamehameha dựa trên hướng của nhân vật
+		newKame.GetComponent<Kamehameha>().SetDirection(Mathf.Sign(transform.localScale.x));
+		Debug.Log("Kamehameha Created at: " + firePoint.position);
+	}
+}
